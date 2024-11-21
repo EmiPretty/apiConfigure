48c13ba94e574f3e6f58e30b2ab5ab6e4b398316
Imene Bentifraouine <bentifraouineimene@gmail.com>
2024-11-21 11:41:14 +0100

    Initial commit for apiConfigure project

---
+++ api/middlewares/authJwt.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/key.js");
+const User = require("../models/user.js");
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!",
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+isExist = async (req, res, next) => {
+  const user = await User.findById(req.userId);
+  if (!user) {
+    res.status(403).send({ message: "User not found" });
+    return;
+  }
+  next();
+};
+
+CheckUser = async (req, res, next) => {
+  const user = await User.findById(req.userId);
+  if (!user) {
+    res.status(403).send({ message: "admin pas trouvé" });
+    return;
+  }
+  if (!user.isadmin) {
+    res.status(403).send({ message: "pas admin" });
+    return;
+  }
+  next();
+};
+
+const authJwt = {
+  verifyToken,
+  isExist,
+  CheckUser
+};
+module.exports = authJwt;
